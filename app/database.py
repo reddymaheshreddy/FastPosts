704f709ff03d1528173f6d62dfd8ad68e0119152
Reddy Mahesh Reddy Dugganapalli <reddymaheshreddydugganapalli@Reddys-MacBook-Air.local>
2025-07-16 14:29:34 -0400

    minor changes to databse

--- app/database.py
+++ app/database.py
@@ -3,7 +3,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+
 SQLALCHEMY_DATABASE_URL = os.environ["DATABASE_URL"]
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL= SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
